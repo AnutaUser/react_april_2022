aa00462f00848137cc17e13c69618a5acf6adba1
AnutaUser <bezkorovainaanna048@gmail.com>
2022-05-04 12:13:16 +0300

    lesson9_typescript

---
+++ dist/enums/my.enum.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.MyEnum = void 0;
+var MyEnum;
+(function (MyEnum) {
+    MyEnum["add"] = "add";
+    MyEnum["delete"] = "delete";
+    MyEnum["update"] = "update";
+})(MyEnum = exports.MyEnum || (exports.MyEnum = {}));
+//# sourceMappingURL=my.enum.js.map
